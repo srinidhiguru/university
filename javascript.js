8324f248b3e09331e1bd4e5cbd916f56d77b95a6
Srinidhi Gururajan <srinidhiguru97@gmail.com>
2018-08-27 18:29:51 -0700

    Header fixed

--- javascript.js
+++ javascript.js
@@ -1,8 +1,22 @@
 
-    document.getElementById('toggleMenuBar').addEventListener('click',function(){
-      if(document.getElementById('mainNav').classList.contains('open')){
-        document.getElementById('mainNav').classList.remove('open');
-      } else {
-        document.getElementById('mainNav').classList.add('open');
-      }
-    })
+document.getElementById('toggleMenuBar').addEventListener('click',function(){
+  if(document.getElementById('mainNav').classList.contains('open')){
+    document.getElementById('mainNav').classList.remove('open');
+  } else {
+    document.getElementById('mainNav').classList.add('open');
+  }
+})
+
+/********************************
+    update header on scroll
+*********************************/
+var mainHeader = document.getElementById('mainHeader');
+
+window.addEventListener('scroll', function() {
+  console.log('scrolling');
+  if(window.scrollY > 39) {
+    mainHeader.classList.add('active');
+  } else {
+    mainHeader.classList.remove('active');
+  }
+});
