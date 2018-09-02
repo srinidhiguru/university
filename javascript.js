5405a6be98209f32d82640a4a41a87bbd8a0742e
Srinidhi Gururajan <srinidhiguru97@gmail.com>
2018-09-01 18:20:26 -0700

    simple animations

--- javascript.js
+++ javascript.js
@@ -1,3 +1,7 @@
+
+/********************************
+toggleMenuBar
+*********************************/
 document.getElementById('toggleMenuBar').addEventListener('click',function(){
   if(document.getElementById('mainNav').classList.contains('open')){
     document.getElementById('mainNav').classList.remove('open');
