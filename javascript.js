caf3edff2cbca383b59cd8d9b694eb5feb3a6c10
Srinidhi Gururajan <srinidhiguru97@gmail.com>
2018-09-03 15:15:20 -0700

    smooth scrolling

--- javascript.js
+++ javascript.js
@@ -49,17 +49,17 @@ setInterval(function() {
 /**************************************/
 /*      SMOOTH SCROLL FUNCTION        */
 /**************************************/
-// $(function() {
-//   $('a[href*="#"]:not([href="#"])').click(function() {
-//     if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
-//       var target = $(this.hash);
-//       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-//       if (target.length) {
-//         $('html, body').animate({
-//           scrollTop: target.offset().top - 41+'px'
-//         }, 1000);
-//         return false;
-//       }
-//     }
-//   });
-// });
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top - 41+'px'
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
